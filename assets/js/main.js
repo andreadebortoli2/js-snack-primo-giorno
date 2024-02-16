bd11e8abb8101c7b44636dae148f0cd7b546fe2b
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-02-16 15:41:00 +0100

    add exercise 2/B

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,24 @@
 
 const number = 100;
 
-console.log(number)
+console.log(number);
 
 
 // Esercizio 2
 
-const PI = 3.14;
+// const PI = 3.14;     cambio const in let per l'esercizio 2/B
+let PI = 3.14;
 
-console.log(PI)
+console.log(PI);
 
 
 // Esercizio 2/B
+
+PI = 3.1416;
+
+// cambiando la costante da const a let posso cambiare il valore della costante all'occorrenza
+console.log(PI);
+
+
 // Esercizio 2/C
 // Esercizio 3
